f79c1f473dad8ab3bb12ee29a64599f22f380634
Siim <siim.kirjanen@gmail.com>
2020-09-27 14:40:12 +0300

    removed unused function

--- reg/php/reg_functions.php
+++ reg/php/reg_functions.php
@@ -123,42 +123,6 @@ function getSeatsStats($struct, $bronRegistrations, $takenRegistrations) {
 	return $statsArray;
 }
 
-function seatreg_registration_time_status_reg($startUnix, $endUnix) {
-	$unix = round(microtime(true) * 1000);
-
-	if($startUnix == null && $endUnix == null) {
-		return 'run';
-	}
-
-	if($endUnix == null && $unix > $startUnix) {
-		return 'run';
-	}
-
-	if($endUnix == null && $unix < $startUnix) {
-		return 'wait';
-	}
-
-	if($startUnix == null && $unix < $endUnix) {
-		return 'run';
-	}
-
-	if($startUnix == null && $unix > $endUnix) {
-		return 'end';
-	}
-
-	if($startUnix < $unix && $endUnix > $unix) {
-		return 'run';
-	}
-
-	if($unix > $endUnix) {
-		return 'end';
-	}
-
-	if($startUnix > $unix) {
-		return 'wait';
-	}
-}
-
 function seatreg_get_registration_bookings_reg($code, $show_bookings) {
 	global $wpdb;
 	global $seatreg_db_table_names;
