a33484244cf373709a25357d61dcd0cb92a5182d
Siim <siim.kirjanen@gmail.com>
2021-11-22 17:51:17 +0200

    version change

--- seatreg.php
+++ seatreg.php
@@ -7,7 +7,7 @@
 	Author URI: https://github.com/SiimKirjanen
 	Text Domain: seatreg
 	Domain Path: /languages
-	Version: 1.9.0
+	Version: 1.9.1
 	Requires at least: 5.3
 	Requires PHP: 7.2.28
 	License: GPLv2 or later
