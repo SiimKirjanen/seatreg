31d8eff32909653cf57c22fbf41d6fddb3d766f4
Siim <siim.kirjanen@gmail.com>
2022-04-12 22:16:40 +0300

    added height attr to shortcode

--- php/seatreg_shortcode.php
+++ php/seatreg_shortcode.php
@@ -3,9 +3,10 @@
         $registrations = SeatregRegistrationRepository::getRegistrations();
         $a = shortcode_atts( array(
             'code' =>  (is_array($registrations) && count($registrations)) ? $registrations[0]->registration_code : null,
+            'height' => '700px',
         ), $atts );
         $site_url = get_site_url();
         
-        return "<iframe style='width:100%;height:700px' src='". $site_url ."/?seatreg=registration&c=". $a['code'] ."'></iframe>";
+        return "<iframe style='width:100%;height:". $a['height'] ."' src='". $site_url ."/?seatreg=registration&c=". $a['code'] ."'></iframe>";
     }
     add_shortcode( 'seatreg', 'seatreg_shortcode' );
