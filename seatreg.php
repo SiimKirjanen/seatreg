a20aaabd567a3feced6686918385c65f538f4709
Siim <siim.kirjanen@gmail.com>
2022-10-04 16:59:16 +0300

    Version bump, changelog change

--- seatreg.php
+++ seatreg.php
@@ -6,7 +6,7 @@
 	Author: Siim Kirjanen
 	Author URI: https://github.com/SiimKirjanen
 	Text Domain: seatreg
-	Version: 1.26.0
+	Version: 1.26.1
 	Requires at least: 5.3
 	Requires PHP: 7.2.28
 	License: GPLv2 or later
