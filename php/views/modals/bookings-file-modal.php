5805d90eb98331a11564ef4adb5b9b5e84f91881
Siim <siim.kirjanen@gmail.com>
2023-01-10 15:11:47 +0200

    file modal styles

--- php/views/modals/bookings-file-modal.php
+++ php/views/modals/bookings-file-modal.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="bookings-file-modal" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade bookings-file-modal" id="bookings-file-modal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
