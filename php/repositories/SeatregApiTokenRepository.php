3ee660eaef794a756ea36eeffc5115847e41c550
Siim <siim.kirjanen@gmail.com>
2023-05-25 16:39:34 +0300

    Is API enabled check for API request

--- php/repositories/SeatregApiTokenRepository.php
+++ php/repositories/SeatregApiTokenRepository.php
@@ -5,16 +5,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class SeatregApiTokenRepository {
-
     public static function getApiToken($apiToken) {
         global $seatreg_db_table_names;
 	    global $wpdb;
 
         return $wpdb->get_row( $wpdb->prepare(
-            "SELECT * FROM $seatreg_db_table_names->table_seatreg_api_tokens
-            WHERE api_token = %s",
+            "SELECT a.*, b.public_api_enabled 
+            FROM $seatreg_db_table_names->table_seatreg_api_tokens AS a
+            INNER JOIN $seatreg_db_table_names->table_seatreg_options AS b
+            ON a.registration_code = b.registration_code
+            WHERE a.api_token = %s",
             $apiToken
         ) );
+       
     }
 
     public static function getRegistrationApiTokens($registrationCode) {
--- php/services/SeatregPublicApiService.php
+++ php/services/SeatregPublicApiService.php
@@ -6,45 +6,59 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class SeatregPublicApiService {
 
+    /**
+     *
+     * Validate public API request
+     * @param object $request WP_REST_Request
+     * @return WP_Error|object Returns WP_Error when validation fails. Returns API token object when validation succeeds
+     * 
+    */
+    public static function validateApiRequest(WP_REST_Request $request) {
+        $apiTokenParam = $request->get_param( 'api_token' );
+
+        if( !$apiTokenParam ) {
+            return new WP_Error( 'no_token', 'Token not provided', array( 'status' => 401 ) );
+        }
+
+        $apiToken = SeatregApiTokenRepository::getApiToken($apiTokenParam);
+
+        if( !$apiToken->public_api_enabled ) {
+            return new WP_Error( 'public_api_not_enabled', 'Public API not enabled', array( 'status' => 403 ) );
+        }
+
+        if( !$apiToken ) {
+            return new WP_Error( 'token_not_found', 'Token not found', array( 'status' => 401 ) );
+        }
+
+        return $apiToken;
+    }
+
     public static function echo( WP_REST_Request $request ) {
         return (object) ['message' => SEATREG_API_OK_MESSAGE];
     }
 
     public static function validateToken( WP_REST_Request $request ) {
-        $apiTokenParam = $request->get_param( 'api_token' );
+        $apiTokenOrError = self::validateApiRequest($request);
 
-        if( !$apiTokenParam ) {
-            return new WP_Error( 'no_token', 'Token not provided', array( 'status' => 401 ) );
+        if( is_wp_error( $apiTokenOrError ) ) {
+            return $apiTokenOrError;
         }
-        
-        $apiToken = SeatregApiTokenRepository::getApiToken($apiTokenParam);
-
-        if( !$apiToken ) {
-            return new WP_Error( 'token_not_found', 'Token not found', array( 'status' => 401 ) );
-        }
-
         return (object) [
             'message' => SEATREG_API_OK_MESSAGE,
             'siteUrl' => get_site_url(),
-            'apiToken' => $apiToken->api_token,
-            'id' => $apiToken->id,
+            'apiToken' => $apiTokenOrError->api_token,
+            'id' => $apiTokenOrError->id,
         ];
     }
 
     public static function getBookings( WP_REST_Request $request ) {
-        $apiTokenParam = $request->get_param( 'api_token' );
+        $apiTokenOrError = self::validateApiRequest($request);
 
-        if( !$apiTokenParam ) {
-            return new WP_Error( 'no_token', 'Token not provided', array( 'status' => 401 ) );
+        if( is_wp_error( $apiTokenOrError ) ) {
+            return $apiTokenOrError;
         }
 
-        $apiToken = SeatregApiTokenRepository::getApiToken($apiTokenParam);
-
-        if( !$apiToken ) {
-            return new WP_Error( 'token_not_found', 'Token not found', array( 'status' => 401 ) );
-        }
-
-        $bookings = SeatregBookingRepository::getConfirmedAndApprovedBookingsByRegistrationCode($apiToken->registration_code);
+        $bookings = SeatregBookingRepository::getConfirmedAndApprovedBookingsByRegistrationCode($apiTokenOrError->registration_code);
 
         return (object) [
             'message' => SEATREG_API_OK_MESSAGE,
