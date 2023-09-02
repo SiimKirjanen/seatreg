a42d5aa82d508b55a4255fb9887c6ab8e40928d7
Siim <siim.kirjanen@gmail.com>
2023-09-02 11:01:04 +0300

    added license details to main plugin file

--- seatreg.php
+++ seatreg.php
@@ -12,6 +12,18 @@
 	License: GPLv2 or later
 */
 
+/*
+SeatReg is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+SeatReg is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+*/
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; 
 }
