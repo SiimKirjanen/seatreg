9b00276670bc09de80efe34aa336d3ca1f681352
Siim <siim.kirjanen@gmail.com>
2023-10-11 13:20:49 +0300

    Booking PDF display restrictions

--- php/pages/booking_pdf.php
+++ php/pages/booking_pdf.php
@@ -5,6 +5,20 @@
 
 require_once( SEATREG_PLUGIN_FOLDER_DIR . 'php/bookings/SeatregBookingPDF.php' );
 
+function shouldAllowPdfGeneration($bookings, $bookingData) {
+	if(!$bookings) {
+		return false;
+	}
+
+	if( $bookings[0]->status === '1' &&  $bookingData->show_pending_booking_pdf === '0' ) {
+		return false;
+	}else if( $bookings[0]->status === '2' && $bookingData->show_approved_booking_pdf === '0' ) {
+		return false;
+	}
+
+	return true;
+}
+
 if ( !defined( 'ABSPATH' ) ) {
 	exit(); 
 }
@@ -16,7 +30,7 @@ if( empty( $_GET['id'] )  ) {
 $bookings = SeatregBookingRepository::getBookingsById( $_GET['id'] );
 $bookingData = SeatregBookingRepository::getDataRelatedToBooking( $_GET['id'] );
 
-if( !$bookings ) {
+if( !$bookings || !shouldAllowPdfGeneration($bookings, $bookingData) ) {
 	esc_html_e('Booking not found or feature disabled', 'seatreg');
 
 } else {
