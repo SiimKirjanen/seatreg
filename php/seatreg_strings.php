bf4be4f58348646e716b6bb119bc0bf829cfd29e
Siim <siim.kirjanen@gmail.com>
2024-02-13 15:33:41 +0200

    Please log in notification messages in registration view when needed

--- php/seatreg_strings.php
+++ php/seatreg_strings.php
@@ -66,7 +66,8 @@ function seatreg_generate_registration_strings() {
     $translations->passwordNotCorrect = esc_html__('Password is not correct', 'seatreg');
     $translations->closedPleaseChooseNewDate = esc_html__('Registration not open for today. Please choose another date', 'seatreg');
     $translations->controlledZoom = esc_html__('Hold Z key also to zoom', 'seatreg');
-
+    $translations->wpLoginRequired = esc_html__('Please log in to make a booking', 'seatreg');
+    
 	return $translations;
 }
 
