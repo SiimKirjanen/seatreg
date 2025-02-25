9cf1abe7ab06e249df4644fb97262127848f7628
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2025-02-25 21:13:43 +0200

    Fixed an issue where copying a registration did not include the target registration's custom payment icons

--- php/services/SeatregImageCopyService.php
+++ php/services/SeatregImageCopyService.php
@@ -4,25 +4,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class SeatregImageCopyService {
-    public static function copyRegistrationRoomImages($targetRgistrationCode, $destinationRegistrationCode) {
-        $targetCustomRoomImagesLocation = SeatregUploadsRepository::getCustomRoomImagesLocationDir($targetRgistrationCode);
-        $destinationCustomRoomImagesLocation = SeatregUploadsRepository::getCustomRoomImagesLocationDir($destinationRegistrationCode);
-
-        if ( !file_exists($destinationCustomRoomImagesLocation) ) {
-            $dirCreated = mkdir($destinationCustomRoomImagesLocation, 0755, true);
+    /**
+     * Helper method to copy files from one directory to another.
+     *
+     * @param string $sourceDir The source directory.
+     * @param string $destinationDir The destination directory.
+     * @return bool True on success, false on failure.
+     */
+    private static function copyFiles($sourceDir, $destinationDir) {
+        if ( !file_exists($destinationDir) ) {
+            $dirCreated = mkdir($destinationDir, 0755, true);
 
             if ( !$dirCreated ) {
                 return false;
             }
         }
 
-        $dir = opendir($targetCustomRoomImagesLocation);
+        $dir = opendir($sourceDir);
 
         while (false !== ($file = readdir($dir))) {
             // Skip the current and parent directory entries
             if ($file != '.' && $file != '..') {
                 // Copy each file to the destination directory
-                if (!copy($targetCustomRoomImagesLocation . '/' . $file, $destinationCustomRoomImagesLocation . '/' . $file)) {
+                if (!copy($sourceDir . '/' . $file, $destinationDir . '/' . $file)) {
                     // If any file fails to copy, return false
                     return false;
                 }
@@ -32,5 +36,26 @@ class SeatregImageCopyService {
         closedir($dir);
 
         return true;
+    }
+
+    public static function copyRegistrationRoomImages($targetRegistrationCode, $destinationRegistrationCode) {
+        $targetCustomRoomImagesLocation = SeatregUploadsRepository::getCustomRoomImagesLocationDir($targetRegistrationCode);
+        $destinationCustomRoomImagesLocation = SeatregUploadsRepository::getCustomRoomImagesLocationDir($destinationRegistrationCode);
+
+        return self::copyFiles($targetCustomRoomImagesLocation, $destinationCustomRoomImagesLocation);
+    }
+
+    public static function copyRegistrationPaymentImages($targetRegistrationCode, $destinationRegistrationCode) {
+        $targetCustomRoomImagesLocation = SeatregUploadsRepository::getCustomPaymentIconLocationDir($targetRegistrationCode);
+        $destinationCustomRoomImagesLocation = SeatregUploadsRepository::getCustomPaymentIconLocationDir($destinationRegistrationCode);
+
+        return self::copyFiles($targetCustomRoomImagesLocation, $destinationCustomRoomImagesLocation);
+    }
+
+    public static function copyAllRegistrationImages($targetRegistrationCode, $destinationRegistrationCode) {
+        $roomImagesCopied = self::copyRegistrationRoomImages($targetRegistrationCode, $destinationRegistrationCode);
+        $paymentImagesCopied = self::copyRegistrationPaymentImages($targetRegistrationCode, $destinationRegistrationCode);
+
+        return $roomImagesCopied && $paymentImagesCopied;
     }
 }
