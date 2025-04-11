a24339395bcd271b91f0baf66dec7c8eb7352850
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2025-04-11 14:33:57 +0300

    New setting for one person checkout

--- php/constants.php
+++ php/constants.php
@@ -6,7 +6,7 @@ define('SEATREG_SETTINGS_PAGE', admin_url('/admin.php?page=seatreg-options'));
 define('SEATREG_PAGE_ID', 'seatreg');
 
 // DB
-define('SEATREG_DB_VERSION', '1.44');
+define('SEATREG_DB_VERSION', '1.45');
 
 // Validation
 define('SEATREG_MANAGER_ALLOWED_ORDER', array('id', 'date', 'name', 'room', 'nr', 'payment-status'));
